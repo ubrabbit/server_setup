bbdd4525814669642a79415f7085c9acaed4774c
lvpengxiang <ubrabbit@163.com>
2018-03-02 13:48:36 +0800

    fixbug

---
+++ docker/rabbitmq/debian9/docker-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 设置host和初始账户
+RABBITMQ_USER="rabbitmq"
+RABBITMQ_PASSWORD="rabbitmq"
+HOSTNAME="rabbitmq"
+
+# Create Rabbitmq user
+( sleep 3 ; \
+rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
+rabbitmqctl set_permissions -p ${HOSTNAME} $RABBITMQ_USER  ".*" ".*" ".*" ; \
+echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
+echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
+
+# $@ is used to pass arguments to the rabbitmq-server command.
+# For example if you use it like this: docker run -d rabbitmq arg1 arg2,
+# it will be as you run in the container rabbitmq-server arg1 arg2
+rabbitmq-server $@
